68f503af452cfa965fcad615a1932ea83f29c7ed
Dan Singerman <dansingerman@gmail.com>
2017-03-14 15:40:40 +0000

    add mailgun config and staging env

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'vreel-production.herokuapp.com', protocol: 'https' }
+  config.action_mailer.default_url_options = { host: 'vreel-staging.herokuapp.com', protocol: 'https' }
 
   config.action_mailer.smtp_settings = {
     address: 'smtp.mailgun.org',
