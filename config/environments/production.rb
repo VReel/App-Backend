e5a9571076d53a6e48d5a4021cf781cfe75541e1
Dan Singerman <dansingerman@gmail.com>
2017-03-17 14:50:39 +0000

    re-enable ssl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = false
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
@@ -85,5 +85,5 @@ Rails.application.configure do
     authentication: :plain
   }
 
-  # config.middleware.insert_before ActionDispatch::SSL, Letsencrypt::Middleware
+  config.middleware.insert_before ActionDispatch::SSL, Letsencrypt::Middleware
 end
