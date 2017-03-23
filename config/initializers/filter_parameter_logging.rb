3e62785cc4d25dd988853ebe3cfd1a8289e27e0f
Dan Singerman <dansingerman@gmail.com>
2017-03-23 10:33:36 +0000

    filter more keys and access-token correctly

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -2,4 +2,4 @@
 
 # Configure sensitive parameters which will be filtered from the log file.
 Rails.application.config.filter_parameters += Rails.application.config.filter_parameters +=
-  [:password, :session, :warden, :secret, :salt, :cookie, :csrf, :client, 'access-token']
+  [:password, :session, :warden, :secret, :salt, :cookie, :csrf, :client, :access_token, :vreel_application_id]
