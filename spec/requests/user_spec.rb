a02a02f1a38846700f1f070f8437396d5f08776c
Dan Singerman <dansingerman@gmail.com>
2017-04-06 18:31:15 +0100

    minor tweak

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -3,13 +3,11 @@ require 'rails_helper'
 RSpec.describe 'Users', type: :request do
   before(:each) { create_user_and_sign_in }
   before(:each) { get "/v1/users/#{other_user.id}", headers: auth_headers }
-  let(:other_user) { Fabricate(:user, profile: 'some profile')}
+  let(:other_user) { Fabricate(:user, profile: 'some profile') }
   let(:data) { JSON.parse(response.body) if response.body.present? }
   let(:auth_headers) { auth_headers_from_response }
 
   it 'can view full user details' do
-    get "/v1/users/#{other_user.id}", headers: auth_headers
-
     expect(response.status).to eq 200
     expect(data['data']['id']).to eq other_user.id
     # Ensure we have the full packet details.
