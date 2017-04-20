a7376aab4f3a02377ad9a53daa76a02a50779a7f
Dan Singerman <dansingerman@gmail.com>
2017-04-20 12:58:09 +0100

    migration fix

--- db/migrate/20170406091535_set_post_counts.rb
+++ db/migrate/20170406091535_set_post_counts.rb
@@ -1,7 +1,7 @@
 class SetPostCounts < ActiveRecord::Migration[5.0]
   def change
     User.all.each do |user|
-      user.update_columns(post_count: user.posts.count)
+      user.update_columns(post_count: user.posts.with_deleted.count)
     end
   end
 end
