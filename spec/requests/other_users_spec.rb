9279fb295a682a5f17882f395435365bc3e6d522
Dan Singerman <dansingerman@gmail.com>
2017-04-24 10:21:02 +0100

    rename spec

---
+++ spec/requests/other_users_spec.rb
@@ -0,0 +1,115 @@
+require 'rails_helper'
+
+RSpec.describe 'Other users', type: :request do
+  let!(:user) { create_user_and_sign_in }
+  let(:gandalf) { Fabricate(:user) }
+  let(:data) { JSON.parse(response.body) }
+  let(:next_response_data) { JSON.parse(response.body) }
+  let(:total_records) { more_than_a_page_count }
+  let(:auth_headers) { auth_headers_from_response }
+
+  describe 'user full details' do
+    before(:each) do
+      get "/v1/users/#{gandalf.id}", headers: auth_headers
+    end
+
+    it 'can get user details' do
+      expect(response.status).to eq 200
+      expect(data['data']['id']).to eq gandalf.id
+    end
+
+    it 'shows if I follow the user' do
+      expect(data['data']['attributes']['followed_by_me']).to be false
+
+      user.follow(gandalf)
+
+      get "/v1/users/#{gandalf.id}", headers: auth_headers
+
+      expect(next_response_data['data']['attributes']['followed_by_me']).to be true
+    end
+
+    it 'shows if the user follows me' do
+      expect(data['data']['attributes']['follows_me']).to be false
+
+      gandalf.follow(user)
+
+      get "/v1/users/#{gandalf.id}", headers: auth_headers
+
+      expect(next_response_data['data']['attributes']['follows_me']).to be true
+    end
+  end
+
+  describe 'list posts' do
+    before(:each) do
+      total_records.times { fabricate_post_for(gandalf) }
+
+      get "/v1/users/#{gandalf.id}/posts", headers: auth_headers
+    end
+
+    it 'gets the most recent post first' do
+      expect(data['data'].first['id']).to eq Post.all.order('created_at desc').first.id
+    end
+
+    it 'gets a page of posts' do
+      first_page_expectations
+    end
+
+    it 'gets the next page of posts' do
+      next_page_expectations(total: total_records)
+    end
+  end
+
+  describe 'list liked posts' do
+    before(:each) do
+      total_records.times do
+        gandalf.like(fabricate_post_for(Fabricate(:user)))
+      end
+
+      get "/v1/users/#{gandalf.id}/likes", headers: auth_headers
+    end
+
+    it 'can get a page of posts liked by a user' do
+      first_page_expectations
+    end
+
+    it 'can get the next page' do
+      next_page_expectations(total: total_records)
+    end
+  end
+
+  describe 'followers' do
+    before(:each) do
+      total_records.times do
+        Fabricate(:user).follow(gandalf)
+      end
+
+      get "/v1/users/#{gandalf.id}/followers", headers: auth_headers
+    end
+
+    it 'can get a page of followers' do
+      first_page_expectations
+    end
+
+    it 'can get the next page' do
+      next_page_expectations(total: total_records)
+    end
+  end
+
+  describe 'following' do
+    before(:each) do
+      total_records.times do
+        gandalf.follow(Fabricate(:user))
+      end
+
+      get "/v1/users/#{gandalf.id}/following", headers: auth_headers
+    end
+
+    it 'can get a page of followed users' do
+      first_page_expectations
+    end
+
+    it 'can get the next page' do
+      next_page_expectations(total: total_records)
+    end
+  end
+end
