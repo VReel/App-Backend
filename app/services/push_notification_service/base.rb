cca1772bfdc9e62c56752c76e26ad3fe19bce08e
Dan Singerman <dansingerman@gmail.com>
2017-06-01 11:17:08 +0100

    Send only valid player ids (#64)
    * validate player ids on sending
    
    * ensure we do not call one signal if there are no valid ids

--- app/services/push_notification_service/base.rb
+++ app/services/push_notification_service/base.rb
@@ -2,11 +2,19 @@ module PushNotificationService
   class Base
     ONE_SIGNAL_BATCH_SIZE = 2000
 
+    # This should match any RFC4122 UUID
+    PLAYER_ID_REGEX = /\A[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}\z/i
+
     def query_batch_size
       Integer(ENV['ONE_SIGNAL_BATCH_SIZE'] || ONE_SIGNAL_BATCH_SIZE)
     end
 
     def send_notification(player_ids, params)
+      # Ensure we only send valid player_ids, so the One Signal does not error.
+      player_ids = player_ids.select { |id| id[PLAYER_ID_REGEX] }
+
+      return if player_ids.none?
+
       OneSignal::Notification.create(params: {
         app_id: ENV['ONE_SIGNAL_APP_ID'],
         include_player_ids: player_ids.first(ONE_SIGNAL_BATCH_SIZE)
