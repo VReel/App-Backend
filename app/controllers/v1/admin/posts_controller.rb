296d20570bca3b03653a4dfdd3fc5fc22afc0822
Dan Singerman <dansingerman@gmail.com>
2017-06-07 15:49:06 +0100

    allow more foltering

--- app/controllers/v1/admin/posts_controller.rb
+++ app/controllers/v1/admin/posts_controller.rb
@@ -18,12 +18,19 @@ class V1::Admin::PostsController < V1::PublicTimelineController
     }
   end
 
+  # rubocop:disable Metrics/AbcSize
   def query
     q = Post.all
-    q.where!('created_at >= ?', Time.zone.parse(params[:date_from]).beginning_of_day) if params[:date_from].present?
-    q.where!('created_at <= ?', Time.zone.parse(params[:date_to]).end_of_day) if params[:date_to].present?
+    q.where!('posts.created_at >= ?', Time.zone.parse(params[:date_from]).beginning_of_day) if params[:date_from].present?
+    q.where!('posts.created_at <= ?', Time.zone.parse(params[:date_to]).end_of_day) if params[:date_to].present?
+    q.where!('posts.user_id IN (SELECT id FROM users WHERE handle = ?)', params[:user]) if params[:user].present?
+    q.where!('posts.like_count >= ?', params[:min_likes].to_i) if params[:min_likes].present?
+    q.where!('posts.like_count <= ?', params[:max_likes].to_i) if params[:max_likes].present?
+    q.where!('posts.comment_count >= ?', params[:min_comments].to_i) if params[:min_comments].present?
+    q.where!('posts.comment_count <= ?', params[:max_comments].to_i) if params[:max_comments].present?
     q.order(order_clause.join(' '))
   end
+  # rubocop:enable Metrics/AbcSize
 
   def order_clause
     case params[:sort]
