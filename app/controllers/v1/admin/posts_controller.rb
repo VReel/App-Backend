d6f00cfdb61ad3f22119587016074bf56915e438
Dan Singerman <dansingerman@gmail.com>
2017-06-07 11:45:30 +0100

    add concern for admin pagination

--- app/controllers/v1/admin/posts_controller.rb
+++ app/controllers/v1/admin/posts_controller.rb
@@ -1,31 +1,14 @@
 class V1::Admin::PostsController < V1::PublicTimelineController
+  include AdminPagination
   before_action :authenticate_chief!
 
   protected
-
-  def paginate(query)
-    query.limit!(API_PAGE_SIZE + 1)
-    query.offset!(current_page * API_PAGE_SIZE)
-    query
-  end
 
   def posts
     return @posts unless @posts.nil?
     @posts = query.includes(:user)
     paginate(@posts)
     @posts
-  end
-
-  def next_page_id
-    @next_page_id ||= current_page + 1
-  end
-
-  def current_page
-    params[:page].present? ? Integer(params[:page]) : 0
-  end
-
-  def meta
-    super.merge(total: query.dup.count)
   end
 
   def posts_links
