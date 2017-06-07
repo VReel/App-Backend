33088cbac120dcf07bbe0962561fe1df04794533
Dan Singerman <dansingerman@gmail.com>
2017-06-07 17:37:22 +0100

    make post search by handle case insensitive

--- app/controllers/v1/admin/posts_controller.rb
+++ app/controllers/v1/admin/posts_controller.rb
@@ -33,7 +33,7 @@ class V1::Admin::PostsController < V1::Admin::BaseController
     q = Post.all
     q.where!('posts.created_at >= ?', Time.zone.parse(params[:date_from]).beginning_of_day) if params[:date_from].present?
     q.where!('posts.created_at <= ?', Time.zone.parse(params[:date_to]).end_of_day) if params[:date_to].present?
-    q.where!('posts.user_id IN (SELECT id FROM users WHERE handle = ?)', params[:user]) if params[:user].present?
+    q.where!('posts.user_id IN (SELECT id FROM users WHERE handle ilike(?))', params[:user]) if params[:user].present?
     q.where!('posts.like_count >= ?', params[:min_likes].to_i) if params[:min_likes].present?
     q.where!('posts.like_count <= ?', params[:max_likes].to_i) if params[:max_likes].present?
     q.where!('posts.comment_count >= ?', params[:min_comments].to_i) if params[:min_comments].present?
